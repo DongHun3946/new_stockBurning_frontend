7eb512320fc7e6de5c08f3aea93445ecfd4a873f
DongHun Choi <a3989957@naver.com>
2025-03-05 20:26:06 +0900

    MODIFY : vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-      })
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
     ],
   },
   devServer: {
-    port: 8080 // 원하는 포트 번호로 변경
+    port: 8080
   }
 });
